c2618088a885719af08b44f762a9391917a13a87
jlauderback <102489483+jlauderback@users.noreply.github.com>
2024-07-17 16:34:25 -0700

    Add files via upload

---
+++ Exercise - 1/src/components/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+function createNotes(noteItem) {
+  return (
+    <Note
+      key={noteItem.key}
+      title={noteItem.title}
+      content={noteItem.content}
+    />
+  );
+}
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map(createNotes)}
+      <Footer />
+    </div>
+  );
+}
